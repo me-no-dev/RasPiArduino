39f64dd5fc220fab26c51ed00a1695d0b5acc62b
Me No Dev <me-no-dev@vdnsbg.com>
2016-01-08 20:05:14 +0200

    Add UDP support

--- cores/piduino/PiUdp.cpp
+++ cores/piduino/PiUdp.cpp
@@ -1,65 +1,137 @@
 #include <PiUdp.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include <netdb.h>
 
 uint8_t PiUDP::begin(uint16_t port){
-  return 0;
+  if(udp_server)
+    stop();
+  if ((udp_server=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+    return 0;
+  }
+  struct sockaddr_in addr;
+  memset((char *) &addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if(bind(udp_server , (struct sockaddr*)&addr, sizeof(addr) ) == -1){
+    udp_server = -1;
+    return 0;
+  }
+  return 1;
 }
 
 void PiUDP::stop(){
-  return;
+  if(udp_server == -1)
+    return;
+  close(udp_server);
+  udp_server = -1;
+}
+
+int PiUDP::beginPacket(){
+  if(!remote_port)
+    return 0;
+  buffer_len = 0;
+  bzero(buffer, 1500);
+  return 1;
 }
 
 int PiUDP::beginPacket(IPAddress ip, uint16_t port){
-  return 0;
+  remote_ip = ip;
+  remote_port = port;
+  return beginPacket();
 }
 
 int PiUDP::beginPacket(const char *host, uint16_t port){
-  return 0;
+  struct hostent *server;
+  server = gethostbyname(host);
+  if (server == NULL)
+    return 0;
+  return beginPacket(IPAddress((const uint8_t *)(server->h_addr_list[0])), port);
 }
 
 int PiUDP::endPacket(){
-  return 0;
+  struct sockaddr_in recipient;
+  recipient.sin_addr.s_addr = (uint32_t)remote_ip;
+  recipient.sin_family = AF_INET;
+  recipient.sin_port = htons(remote_port);
+  return sendto(udp_server, buffer, buffer_len, 0, (struct sockaddr*) &recipient, sizeof(recipient));
 }
 
 size_t PiUDP::write(uint8_t data){
-  return 0;
+  if(buffer_len == 1500){
+    endPacket();
+    buffer_len = 0;
+    bzero(buffer, 1500);
+  }
+  buffer[buffer_len++] = data;
+  return 1;
 }
 
 size_t PiUDP::write(const uint8_t *buffer, size_t size){
-  return 0;
+  size_t i;
+  for(i=0;i<size;i++)
+    write(buffer[i]);
+  return i;
 }
 
 int PiUDP::parsePacket(){
-  return 0;
+  if(rx_buffer)
+    return 0;
+  struct sockaddr_in si_other;
+  int slen = sizeof(si_other) , len;
+  char buf[1500];
+  bzero(buf, 1500);
+  if ((len = recvfrom(udp_server, buf, 1500, MSG_DONTWAIT, (struct sockaddr *) &si_other, (socklen_t *)&slen)) == -1){
+    return 0;
+  }
+  remote_ip = IPAddress(si_other.sin_addr.s_addr);
+  remote_port = ntohs(si_other.sin_port);
+  rx_buffer = new cbuf(len);
+  rx_buffer->write(buf, len);
+  return len;
 }
 
 int PiUDP::available(){
-  return 0;
+  return rx_buffer->getSize();
 }
 
 int PiUDP::read(){
-  return 0;
+  int out = rx_buffer->read();
+  if(!rx_buffer->getSize()){
+    delete rx_buffer;
+    rx_buffer = 0;
+  }
+  return out;
 }
 
 int PiUDP::read(unsigned char* buffer, size_t len){
-  return 0;
+  return read((char *)buffer, len);
 }
 
 int PiUDP::read(char* buffer, size_t len){
-  return 0;
+  int out = rx_buffer->read(buffer, len);
+  if(!rx_buffer->getSize()){
+    delete rx_buffer;
+    rx_buffer = 0;
+  }
+  return out;
 }
 
 int PiUDP::peek(){
-  return 0;
+  return rx_buffer->peek();
 }
 
 void PiUDP::flush(){
-  return;
+  rx_buffer->flush();
+  delete rx_buffer;
+  rx_buffer = 0;
 }
 
 IPAddress PiUDP::remoteIP(){
-  return IPAddress((uint32_t)0);
+  return remote_ip;
 }
 
 uint16_t PiUDP::remotePort(){
-  return 0;
+  return remote_port;
 }
--- cores/piduino/PiUdp.h
+++ cores/piduino/PiUdp.h
@@ -3,12 +3,26 @@
 
 #include <Arduino.h>
 #include <Udp.h>
+#include <cbuf.h>
 
 class PiUDP : public UDP {
-
+private:
+  int udp_server;
+  IPAddress remote_ip;
+  uint16_t remote_port;
+  char buffer[1500];
+  size_t buffer_len;
+  cbuf * rx_buffer;
 public:
+  PiUDP():udp_server(-1), remote_port(0), buffer_len(0), rx_buffer(0){}
+  ~PiUDP(){
+    stop();
+    if(rx_buffer)
+      delete rx_buffer;
+  }
   uint8_t begin(uint16_t);
   void stop();
+  int beginPacket();
   int beginPacket(IPAddress ip, uint16_t port);
   int beginPacket(const char *host, uint16_t port);
   int endPacket();
