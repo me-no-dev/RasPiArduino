122996bb125c57a46f69e490d38ea824433a2ab6
Me No Dev <me-no-dev@vdnsbg.com>
2016-01-08 02:35:44 +0200

    Use hardware TX fifo for UART

--- cores/piduino/HardwareSerial.cpp
+++ cores/piduino/HardwareSerial.cpp
@@ -28,24 +28,20 @@
 #define UART_BUFFER_SIZE 1024
 
 volatile char uart_rx_buffer[UART_BUFFER_SIZE];
-volatile char uart_tx_buffer[UART_BUFFER_SIZE];
 volatile int uart_rx_buffer_index = 0;
-volatile int uart_tx_buffer_index = 0;
-volatile uint8_t uart_thread_running = 0;
 
 //called by the interrupt check thread
 void uart_check_fifos(){
   if((UART0CR & _BV(UART0EN)) != 0){
     while((UART0FR & _BV(UART0RXFE)) == 0 && uart_rx_buffer_index < UART_BUFFER_SIZE)
       uart_rx_buffer[uart_rx_buffer_index++] = UART0DR;
-    while(uart_tx_buffer_index && (UART0FR & _BV(UART0TXFF)) == 0) {
-      uint8_t data = uart_tx_buffer[0];
-      uart_tx_buffer_index--;
-      int i;
-      for(i = 0; i < uart_tx_buffer_index; i++) uart_tx_buffer[i] = uart_tx_buffer[i+1];
-      UART0DR = data;
-    }
   }
+}
+
+size_t HardwareSerial::write(uint8_t c){
+  while(UART0FR & _BV(UART0TXFF));
+  UART0DR = c;
+  return 1;
 }
 
 void HardwareSerial::begin(uint32_t baud){
@@ -83,13 +79,6 @@ int HardwareSerial::read(void){
 
 void HardwareSerial::flush(){
   uart_rx_buffer_index = 0;
-  uart_tx_buffer_index = 0;
-}
-
-size_t HardwareSerial::write(uint8_t c){
-  if(uart_tx_buffer_index < UART_BUFFER_SIZE)
-    uart_tx_buffer[uart_tx_buffer_index++] = c;
-  return 1;
 }
 
 #ifdef SERIAL_TO_CONSOLE
--- cores/piduino/IPAddress.h
+++ cores/piduino/IPAddress.h
@@ -29,8 +29,8 @@
 class IPAddress : public Printable {
 private:
     union {
-	uint8_t bytes[4];  // IPv4 address
-	uint32_t dword;
+      uint8_t bytes[4];  // IPv4 address
+      uint32_t dword;
     } _address;
 
     // Access the raw byte array containing the address.  Because this returns a pointer
@@ -42,6 +42,7 @@ private:
 public:
     // Constructors
     IPAddress();
+    virtual ~IPAddress(){}
     IPAddress(uint8_t first_octet, uint8_t second_octet, uint8_t third_octet, uint8_t fourth_octet);
     IPAddress(uint32_t address);
     IPAddress(const uint8_t *address);
@@ -73,6 +74,6 @@ public:
     friend class DNSClient;
 };
 
-const IPAddress INADDR_NONE(0,0,0,0);
+//const IPAddress INADDR_NONE(0,0,0,0);
 
 #endif
