b918d58d45852818842997e4232a5114fe47e9c4
Patrick Fink <mail@pfink.de>
2021-01-16 16:51:28 +0100

    Fix UDP rx buffer overflow
    Co-Authored-By: Christoph Schulz <public-github@criztovyl.space>

--- libraries/Bridge/src/BridgeUdp.cpp
+++ libraries/Bridge/src/BridgeUdp.cpp
@@ -87,7 +87,7 @@ int BridgeUDP::parsePacket(){
   }
   remote_ip = IPAddress(si_other.sin_addr.s_addr);
   remote_port = ntohs(si_other.sin_port);
-  rx_buffer = new cbuf(len);
+  rx_buffer = new cbuf(len+1);
   rx_buffer->write(buf, len);
   return len;
 }
